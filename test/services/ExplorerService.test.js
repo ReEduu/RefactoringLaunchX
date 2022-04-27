08b19958c2b73a2a2abe5b044828457cad7ff69a
joseduardo <reedu_@outlook.com>
2022-04-27 18:39:27 -0500

    Se completo la parte de Refactoring

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,14 @@
+const Reader = require('./../../lib/utils/Reader')
+const ExplorerService = require('./../../lib/services/ExplorerService')
+
+describe("Unit test for User Reader",()=>{
+    test('Return the list of explorers antes test the first explorer',()=>{
+        const explorers = Reader.readJsonFile("explorers.json")
+        nodeExplorers = ExplorerService.filterByMission(explorers, "node")
+        nodeAmount = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        nodeUsernames=ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        expect(nodeExplorers[0].name).toBe("Woopa1")
+        expect(nodeAmount).toBe(10)
+        expect(nodeUsernames[0]).toBe("ajolonauta1")
+    })
+})
---
+++ test/utils/Reader.test.js
@@ -0,0 +1,12 @@
+const Reader = require('./../../lib/utils/Reader')
+
+describe("Unit test for User Reader",()=>{
+    test('Return the list of explorers antes test the first explorer',()=>{
+        const explorers = Reader.readJsonFile('explorers.json')
+        expect(explorers[0].name).toBe("Woopa1")
+        expect(explorers[0].githubUsername).toBe("ajolonauta1")
+        expect(explorers[0].score).toBe(1)
+        expect(explorers[0].mission).toBe("node")
+        expect(explorers[0].stacks[0]).toBe("javascript")
+    })
+})
